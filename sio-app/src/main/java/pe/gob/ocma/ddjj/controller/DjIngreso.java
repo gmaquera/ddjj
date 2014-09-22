0ae533c57c388d342807f511f797edab83a31b3d
gmaquera <ginux@outlook.com>
2014-09-22 16:12:07 -0500

    1

---
+++ sio-app/src/main/java/pe/gob/ocma/ddjj/controller/DjIngreso.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright © 2013 OCMA.
+ *
+ * Este es el sistema para la gestión integrada de los procesos de
+ * la OCMA.
+ *
+ * Este sistema es de propiedad de la Oficina de Control de la 
+ * Magistratura - OCMA, No puede ser distribuido ni modificado
+ * sin autorización.
+ *
+ * Desarrollado en Mayo del 2013, por la Unidad de Sistemas de la OCMA.
+ * Ing. Orlando Peña Mora - Responsable de la Unidad de Sistemas
+ *
+ */
+package pe.gob.ocma.ddjj.controller;
+
+/**
+ * FIXME DESCRIBIR
+ * @author Gino Maquera Coyla
+ * 22/9/2014 16:11:26
+ */
+public class DjIngreso {
+
+}
